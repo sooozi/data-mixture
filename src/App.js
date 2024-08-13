f1191eed9d886e576a0d66a97b6d36280d2a4174
Suzy Kim <skme1103@naver.com>
2024-08-13 15:24:34 +0900

    Add computer function

--- src/App.js
+++ src/App.js
@@ -18,14 +18,24 @@ const choice = {
 }
 function App() {
   const [userSelect, setUserSelect] = useState(null);
+  const [randomSelect, setRandomSelect] = useState(null);
   const play = (userChoice) => {
     setUserSelect(choice[userChoice])
+    showRandom();
   }
+
+  function showRandom() {
+    const choiceKey = Object.keys(choice);
+    const randomImg = choiceKey[Math.floor(Math.random() * choiceKey.length)];
+    setRandomSelect(choice[randomImg]);
+    console.log(randomImg)
+  }
+
   return (
     <div className="App">
       <div className="cont-wrap">
         <RpsBox title="🙋‍♀️ YOU" item={userSelect}/>
-        {/* <RpsBox title="👩‍💻 COMPUTER"/> */}
+        <RpsBox title="👩‍💻 COMPUTER" item={randomSelect}/>
       </div>
       <div className="cont-wrap">
         <button onClick={() => play("scissors")}>✌️</button>
