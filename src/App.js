414eaed33ebc8d437357d57ad794b17262c7c4df
Suzy Kim <skme1103@naver.com>
2024-08-17 15:52:17 +0900

    Get Api info on me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const API_KEY = process.env.REACT_APP_WEATHER_KEY;
 
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeather(lat, lon);
+    });
+  }
+  const getWeather = async(lat, lon) => {
+    try {
+      const link = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      
+      )
+      console.log(link)
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, [])
   return (
     <div className="App">
 
