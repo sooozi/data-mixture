8064081cacc15bb92b2e83e35927f12c99ffb7e3
Suzy Kim <skme1103@naver.com>
2024-08-17 21:40:36 +0900

    fix error

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -5,8 +5,8 @@ const WeatherButton = ({cities}) => {
   return (
     <div className='weather-btn-wrap'>
       <Button variant="secondary">Here!</Button>
-      {cities.map((item) => (
-        <Button variant="secondary">{item}</Button>
+      {cities.map(item => (
+        <Button variant="secondary" key={item}>{item}</Button>
       ))}
     </div>
   )
