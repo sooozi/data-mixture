963617a94ff5ca27a7f75c136b70969325e3e0da
Suzy Kim <skme1103@naver.com>
2024-08-17 17:42:14 +0900

    Fix http, https error

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   }
 
   const getWeatherByCurrentLocation = async(lat, lon)  => {
-    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
