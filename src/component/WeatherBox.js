c8ce851aec1f7d7e2c85f63dbb13d18f0657a87f
Suzy Kim <skme1103@naver.com>
2024-08-17 21:32:55 +0900

    Fix style

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -5,9 +5,10 @@ const WeatherBox = ({weather}) => {
     return (
         <div className='weather-box-wrap'>
             {/*삼항연산식 */}
-            <span>{weather?.name}</span> 
-            <h3>{weather?.main.temp}℃ / {(weather?.main.temp * 1.8 + 32).toFixed(2)}℉</h3>
-            <h4>{weather?.weather[0].description}</h4>
+            <span className="txt-location">{weather?.name}</span> 
+            <div className="icon-box"><img src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}.png`} alt="Weather Icon" /></div>
+            <h3 className="txt-temp">{weather?.main.temp}℃ / {(weather?.main.temp * 1.8 + 32).toFixed(2)}℉</h3>
+            <h5 className="txt-weather-desc">{weather?.weather[0].description}</h5>
         </div>
     )
 }
