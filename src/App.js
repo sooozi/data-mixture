7a3820983eebedd418ee7ed1e07acb4be2f4258f
Suzy Kim <skme1103@naver.com>
2024-08-21 13:15:56 +0900

    Add Nav component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
+import NAV from './component/Nav';
 import BoxPage from './page/BoxPage';
 import HomePage from './page/HomePage';
 import Login from './page/Login';
@@ -11,8 +12,14 @@ import RpsPage from './page/RpsPage';
 import WeatherAppPage from './page/WeatherAppPage';
 
 function App() {
+  const location = useLocation();
+  
+  //location.pathname.startsWith(path) : 경로가 해당 문자열로 시작하는지 확인
+  const showNav = ["/login", "/shop", "/product"].some(path => location.pathname.startsWith(path));
+
   return (
     <div className="App">
+      {showNav && <NAV />}
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/BoxPage" element={<BoxPage/>}></Route>
---
+++ src/component/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <h1>네비게이션 컴포넌트</h1>
+    </div>
+  )
+}
+
+export default Nav
