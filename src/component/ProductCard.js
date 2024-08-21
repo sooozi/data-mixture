f0a0bb7c05f6dbec87e696fbfa2771b41f405d85
Suzy Kim <skme1103@naver.com>
2024-08-21 14:03:41 +0900

    Add data

---
+++ src/component/ProductCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src="https://cdn.imweb.me/thumbnail/20230513/d18e929c5c72c.png" alt="" />
+        <span>Pear Paperweight - petite</span>
+        <span>38,000</span>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,12 +1,14 @@
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from 'react';
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
   return (
     <div>
         <h1>전체 상품 페이지</h1>
         <FontAwesomeIcon icon={faHouse} />
+        <ProductCard />
     </div>
   )
 }
