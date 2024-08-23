3a9a34a22fcbfbe4c54eb48fb56ff2ccd2ce2c8b
Suzy Kim <skme1103@naver.com>
2024-08-23 15:53:51 +0900

    test4

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,33 +7,25 @@ import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
   let [productList, setProductList] = useState([]);
-  // const [query, setQuery] = useSearchParams();
   const [query] = useSearchParams();  // 쿼리 파라미터를 읽기만 함
-  const getProducts = async () => {
+
+  const getProducts = useCallback(async () => {
     let searchQuery = query.get("q") || "";
     let url = `https://my-json-server.typicode.com/sooozi/data-mixture/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
-  };
-  // console.log(setQuery);
-  // const getProducts = async() => {
-  //   let searchQuery = query.get("q") || "";
-  //   let url =`https://my-json-server.typicode.com/sooozi/data-mixture/products?q=${searchQuery}`;
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   setProductList(data);
-  // };
+  }, [query]);  // query 객체가 변경될 때마다 호출됨
 
   useEffect(() => {
     getProducts();
-  }, [query] );
+  }, [getProducts]);  // getProducts 함수가 변경될 때마다 호출됨
 
   return (
     <div className="page-productAll">
       <Container>
         <Row>
-          {productList.map((item )=> (
+          {productList.map((item) => (
             <Col lg={3} key={item.id} className='col-lg-3'>
               <ProductCard item={item}/>
             </Col>
@@ -41,7 +33,7 @@ const ProductAll = () => {
         </Row>
       </Container>
     </div>
-  )
+  );
 }
 
-export default ProductAll
+export default ProductAll;
