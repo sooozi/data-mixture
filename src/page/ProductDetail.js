94fb570d2d748a6a64b805a569b72f47afff2bf6
Suzy Kim <skme1103@naver.com>
2024-08-23 15:07:42 +0900

    first commit

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -8,7 +8,7 @@ const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const getProductDetail = async() => {
-    let url = `http://localhost:3000/products/${id}`;
+    let url =`https://my-json-server.typicode.com/sooozi/data-mixture/products/${id}`;
     let response = await fetch(url);
     let data =  await response.json();
     setProduct(data);
