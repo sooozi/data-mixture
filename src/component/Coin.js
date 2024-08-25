6fe326d14ef51eb5c470c7f83e2c663f8d24bce8
Suzy Kim <skme1103@naver.com>
2024-08-25 23:21:31 +0900

    Add style

---
+++ src/component/Coin.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Coin = () => {
+  return (
+    <div className="coin-wrap">
+        <div className="coin">
+            <div className="front jump">
+                <span className="currency">
+                    $
+                    <div className="shapes">
+                        <span className="top">
+                            noona
+                        </span>
+                        <span className="bottom">
+                            COIN
+                        </span>
+                    </div>
+                </span>
+            </div>
+            <div className="shadow"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Coin
--- src/page/Counting.js
+++ src/page/Counting.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux';
-import ReduxBox from '../component/ReduxBox';
+import Coin from '../component/Coin';
 
 const Counting = () => {
   //state의 count값만 가져온다!
@@ -10,23 +11,38 @@ const Counting = () => {
   const dispatch = useDispatch();
 
   const increase = () => {
-    dispatch({type:"INCREMENT", payload:{num:5}})
+    dispatch({type:"INCREMENT", payload:{num:100}})
   }
   const decrease = () => {
-    dispatch({type:"DECREMENT", payload:{num:5}})
+    dispatch({type:"DECREMENT", payload:{num:100}})
   }
   const login = () => {
-    dispatch({type:"LOGIN", payload:{id:"soozi", password:"1004"}})
+    dispatch({type:"LOGIN", payload:{id:"Hi, I'm participating in the CodingNoona React study group!", password:"Let's keep going and stay strong."}})
   }
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={increase}>증가 ⬆️</button>
-      <button onClick={decrease}>감소 ⬇️</button>
-      <button onClick={login}>로그인 🧙‍♀️</button>
-      <span>ID : {id}</span>
-      <span>PASSWORD : {pw}</span>
-      <ReduxBox />
+    <div className="page-counting">
+      <div className="creator-wrap">
+        <div className="txt-wrap">
+          <span className="txt-white txt-top">{id ? id : 'Click Me! 👉'}</span>
+          <span className="txt-white txt-bottom">{pw}</span>
+        </div>
+        <Button className="btn-creator" onClick={login}>Creator 🧙‍♀️</Button>
+      </div>
+      <div className="coin-wrap">
+        <Coin/> 
+        <h1 className="txt-white">🕹️ Welcome to the Counting App 🕹️</h1>
+        <div className="txt-box">
+          <h5 className="txt-white txt-detail btn-shine">Take as much money as you want!</h5>
+        </div>
+      </div>
+      <div className="btn-wrap">
+        <h2 className="txt-white">{count}</h2>
+        {/* <Button variant="warning" onClick={decrease}>1000 감소 ⬇️</Button> */}
+        <Button variant="warning" onClick={decrease}>100 감소 ⬇️</Button>
+        <Button variant="warning" onClick={increase}>100 증가 ⬆️</Button>
+        {/* <Button variant="warning" onClick={increase}>1000 증가 ⬆️</Button> */}
+      </div>
+        {/* <ReduxBox /> */}
     </div>
   )
 }
